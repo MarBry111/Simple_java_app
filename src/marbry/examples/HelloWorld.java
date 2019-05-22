be2b6f58e053c83a99df5eb1845f906548542e65
MarBry111 <mrmareczek111@gmail.com>
2019-05-22 18:16:58 +0200

    first commit

---
+++ src/marbry/examples/HelloWorld.java
@@ -0,0 +1,17 @@
+package marbry.examples;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        try{
+            System.out.println("Hello, World");
+            for(int i = 3; i>=0; i--) {
+                System.out.println(10/i);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
